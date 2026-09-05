4ae8b6b9dce7880104435958a0770925e8e4f922
NewTechML <dlrmfdkdl5@gamil.com>
2026-09-05 22:52:43 +0900

    bash_operator dag 업로드

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,23 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *", # 분 시 월 일 요일
+    start_date=pendulum.datetime(2026, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    bash_t1 = BashOperator(
+        task_id = "bash_t1",
+        bash_command = "echo whoami"
+    )
+
+    bash_t2 = BashOperator(
+        task_id = "bash_t2",
+        bash_command = "echo $HOSTNAME"
+    )
+
+    bash_t1 >> bash_t2
