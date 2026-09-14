ef108d8bcd907f50e689dba36dc3d72a839511d2
NewTechML <dlrmfdkdl5@gamil.com>
2026-09-14 22:31:17 +0900

    python_operator 구문 수정

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -6,7 +6,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    task_id = "dags_python_operator",
+    dag_id = "dags_python_operator",
     schedule = "30 6 * * *",
     start_date = pendulum.datetime(2026, 1, 1, tz='Asia/Seoul'),
     catchup = False
